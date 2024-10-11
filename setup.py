1c9064500e35d89726ec7c1ed59270fe1000514d
Benit Mulindwa <110304380+Benitmulindwa@users.noreply.github.com>
2024-10-11 23:46:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="flet_canvas2img",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=[
         "Pillow"
